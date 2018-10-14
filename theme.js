c1a700b94697ea54875d6d178ff7ffa6ca1ff2d0
Lenz Weber <mail@lenzw.de>
2018-10-14 21:43:56 +0200

    initial

--- theme.js
+++ theme.js
@@ -1,12 +1,17 @@
-import theme from 'mdx-deck/themes'
+import theme from "mdx-deck/themes";
+import codeSurferTheme from "prism-react-renderer/themes/duotoneLight";
 
 export default {
   ...theme,
   font: '"Source Sans Pro", "Open Sans", Helvetica, Arial',
   colors: {
-    text: '#707372',
-    heading: '#e35205',
-    background: '#fff',
-    link: 'f000',
+    text: "#707372",
+    heading: "#e35205",
+    background: "#fff",
+    link: "f000"
+  },
+  codeSurfer: {
+    ...codeSurferTheme,
+    showNumbers: true
   }
 };
